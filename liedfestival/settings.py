cec5b2d8e08f7e60ab056fef308a4cc92d6ac8d7
Felix Werthschulte <felix@werthschulte.info>
2018-04-05 19:43:36 +0200

    sassc ist eine feine Sache für Farben

--- liedfestival/settings.py
+++ liedfestival/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(BASE_DIR + "/.mysettings", "r") as j:
     mysettings = json.load(j)
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
