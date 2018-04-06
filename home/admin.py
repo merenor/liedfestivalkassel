f90bd81ff7f0d885dbce29e691ec39eda5fe6284
Felix Werthschulte <felix@werthschulte.info>
2018-04-06 21:57:54 +0200

    workshop-anmeldung 2

--- home/admin.py
+++ home/admin.py
@@ -4,6 +4,7 @@ from home.models import *
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(TicketReservation)
+admin.site.register(WorkshopReservation)
 admin.site.register(MailTemplate)
 admin.site.register(Sponsor)
 admin.site.register(Concert)
